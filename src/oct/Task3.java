2572ab1805918be713572eee2f589db2d3d71a62
ASUS <09poonamchaudhari@gmail.com>
2024-10-15 20:18:06 +0530

    04 10 2024

---
+++ src/oct/Task3.java
@@ -0,0 +1,15 @@
+package oct;
+public class Task3 {
+    public static void  main(String []arg){
+        int a=12 ,b=3,add,sub,div,mul;
+        add=a+b;
+        sub=a-b;
+        div=a/b;
+        mul=a*b;
+        System.out.println("Addition="+add);
+        System.out.println("Subtraction="+sub);
+        System.out.println("Division="+div);
+        System.out.println("Multiplication="+mul);
+
+    }
+}
