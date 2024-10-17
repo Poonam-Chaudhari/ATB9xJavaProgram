05884fea8d9324f3560d1806b32011e11d1f66d5
ASUS <09poonamchaudhari@gmail.com>
2024-10-17 18:47:24 +0530

    Task5_161024

---
+++ src/oct/Task5.java
@@ -0,0 +1,13 @@
+package oct;
+
+public class Task5 {
+    public static void main(String [] arg){
+        int a=Integer.parseInt(arg[0]);
+        int b=Integer.parseInt(arg[1]);
+
+        int result=(a>b)?a:b;
+        System.out.println("Greater no. is="+result);
+
+
+    }
+}
